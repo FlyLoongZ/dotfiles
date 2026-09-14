e41d9643f482a6c308a85501fc99bfd6cf2dc2be
FlyLoongZ <60963681+FlyLoongZ@users.noreply.github.com>
2026-09-14 22:15:39 +0800

    chore: 清理不使用的脚本

--- Documents/keepass_bf/bf.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/zsh
-rclone copy jianguoyun-keepass:/dav/keepass /home/c137arch/Documents/keepass_bf && mv /home/c137arch/Documents/keepass_bf/passwd.kdbx /home/c137arch/Documents/keepass_bf/passwd_$(date +%Y.%m.%d_%H:%M).kdbx 2>&1 >> /home/c137arch/Documents/keepass_bf/bf.log
